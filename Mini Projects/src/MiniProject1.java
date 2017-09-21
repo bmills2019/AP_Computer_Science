57586cd3f287052c5e146f1fe6263953174d9be0
bmills2019 <bmills2019@cchsraider.net>
2017-09-21 14:10:05 -0400

    completed project 1

--- Mini Projects/src/MiniProject1.java
+++ Mini Projects/src/MiniProject1.java
@@ -1,9 +1,17 @@
-
+/*
+ * Brandon Mills
+ * Project 1
+ * 
+ * 
+ * 
+ */
 public class MiniProject1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//prints Victory is Mine
+		System.out.println("////////////////////");
+		System.out.println("||Victory is mine!||");
+		System.out.println("////////////////////");
 	}
-
 }
--- Mini Projects/src/MiniProject2.java
+++ Mini Projects/src/MiniProject2.java
@@ -3,7 +3,10 @@ public class MiniProject2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("A well-formed Java program has");
+		System.out.println("a main method { and }");
+		
+	
 	}
 
 }
