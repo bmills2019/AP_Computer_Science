e86ee2c8809bb93b74398fefce6800f258b3c206
bmills2019 <bmills2019@cchsraider.net>
2017-09-21 13:41:09 -0400

    initial commit

---
+++ FightSong/src/FightSong.java
@@ -0,0 +1,90 @@
+
+public class FightSong {
+
+	public static void main(String[] args) {
+		VerseOne();
+
+		VerseTwo();
+
+		VerseThree();
+
+		VerseFour();
+
+		}
+
+		
+
+		public static void VerseOne(){
+
+		GoTeamGo();
+
+		YouCanDoIt();
+
+		}
+
+		public static void VerseTwo(){
+System.out.println("");
+		GoTeamGo();
+
+		YouCanDoIt();
+
+		UrTheBest();
+
+		InTheWest();
+
+		GoTeamGo();
+
+		YouCanDoIt(); 
+
+		}
+
+		public static void VerseThree(){
+			System.out.println("");
+		GoTeamGo();
+
+		YouCanDoIt();
+
+		UrTheBest();
+
+		InTheWest();
+
+		GoTeamGo();
+
+		YouCanDoIt(); 
+
+		}
+
+		public static void VerseFour(){
+			System.out.println("");
+		GoTeamGo();
+
+		YouCanDoIt();
+
+		}
+
+		
+		public static void GoTeamGo() {
+
+		System.out.println("Go, Team, Go!");
+
+		}
+
+		public static void YouCanDoIt() {
+
+		System.out.println("You can do it.");
+
+		}
+
+		public static void UrTheBest() {
+
+		System.out.println("You're the best,");
+
+		}
+
+		public static void InTheWest() {
+
+		System.out.println("In the West.");
+
+	}
+
+}
