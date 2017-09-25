882e7c1cdeba8b0e539280b83e99fff17e87a297
Eric <Eric@192.168.86.32>
2017-09-25 19:05:15 -0400

    Done

--- Mini Projects/src/MiniProject1.java
+++ Mini Projects/src/MiniProject1.java
@@ -2,11 +2,11 @@
  * 
  * Mini Projects 
  * Brandon Mills
- * //prints Victory is Mine
+ * 
  * 
  */
 public class MiniProject1 {
-
+//prints victory is mine
 	public static void main(String[] args) {
 		System.out.println("//////////////////////");
 		System.out.println("|| Victory is mine! ||");
