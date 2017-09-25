a4750f70c3c02e8c5c6a12b9555bdc7a56ee3b89
Eric <Eric@192.168.86.32>
2017-09-25 18:13:28 -0400

    still need to look a five....maybe-nah

--- Mini Projects/src/MinProject10.java
+++ Mini Projects/src/MinProject10.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+//this generates an estimated weight based upon your height
 public class MinProject10 {
 		int Height;
 		double MaleWeight;
@@ -11,11 +11,15 @@ public class MinProject10 {
 		Scanner in=new Scanner(System.in);
 		Double Height=in.nextDouble();
 	
+		if (Height >=60 ) {
 double MaleWeight=(Height-60)*(7)+(106);
 double FemaleWeight=(Height-60)*(6)+(100);
 		System.out.println("Recommended Male Weight: "+MaleWeight);
 		System.out.println("Recommended Female Weight: "+FemaleWeight);
-		
 		}
-	
+		else {
+			System.out.println("Please enter that is 60 inches or higher");
+		//prompts message to change height
+		}
+		}
 }
