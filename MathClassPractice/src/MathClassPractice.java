37885833fbd39af6fd6f28255ade7410caa624d7
Eric <Eric@192.168.86.34>
2017-09-30 11:53:11 -0400

    life is such

--- MathClassPractice/src/MathClassPractice.java
+++ MathClassPractice/src/MathClassPractice.java
@@ -6,21 +6,17 @@ import java.util.Scanner;
 public class MathClassPractice {
 		
 	public static void main(String[] args) {
-		int numOne;
-		int numTwo;
-		int numThree;
-		int partialMax;
-		System.out.println("Enter the first number: ");
-		Scanner scanNum1=new Scanner(System.in);
-		int numOne=in.nextInt();
-		System.out.println("Enter the first number: ");  //prompts user for number input
-		Scanner scanNum2=new Scanner(System.in);
-		int numTwo=in.nextInt();
-		System.out.println("Enter the first number: ");
-		Scanner scanNum3=new Scanner(System.in);
-		int numThree=in.nextInt();
+		int numOne,numTwo,numThree,partialMax;
+		
+		System.out.print("Enter the first number: ");
+		Scanner in=new Scanner(System.in);
+		numOne=in.nextInt();
+		System.out.print("Enter the second number: ");  //prompts user for number input
+		numTwo=in.nextInt();
+		System.out.print("Enter the third number: ");
+		numThree=in.nextInt();
 		in.close();
-		System.out.println(biggestNumberCalc(numOne, numTwo, numThree));//prints biggest number 
+		System.out.println("The biggest number is: " +biggestNumberCalc(numOne, numTwo, numThree));//prints biggest number 
 		}
  public static int biggestNumberCalc(int x, int y, int z) {
 	 int partialmax=Math.max(x, y);//calculates biggest number
