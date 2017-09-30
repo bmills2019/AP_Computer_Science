ad0ffa9181f6a4bfbaf9c4b86a89ae914f4abd6a
Eric <Eric@192.168.86.34>
2017-09-30 11:39:05 -0400

    pls kill me

---
+++ MathClassPractice/src/MathClassPractice.java
@@ -0,0 +1,31 @@
+//Math Class Practice
+//Brandon Mills
+//Calculates the largest number 
+
+import java.util.Scanner;
+public class MathClassPractice {
+		
+	public static void main(String[] args) {
+		int numOne;
+		int numTwo;
+		int numThree;
+		int partialMax;
+		System.out.println("Enter the first number: ");
+		Scanner scanNum1=new Scanner(System.in);
+		int numOne=in.nextInt();
+		System.out.println("Enter the first number: ");  //prompts user for number input
+		Scanner scanNum2=new Scanner(System.in);
+		int numTwo=in.nextInt();
+		System.out.println("Enter the first number: ");
+		Scanner scanNum3=new Scanner(System.in);
+		int numThree=in.nextInt();
+		in.close();
+		System.out.println(biggestNumberCalc(numOne, numTwo, numThree));//prints biggest number 
+		}
+ public static int biggestNumberCalc(int x, int y, int z) {
+	 int partialmax=Math.max(x, y);//calculates biggest number
+	 return Math.max(partialmax,z);
+	 
+	 //pls kill me
+ }
+}
